239e6db58468e122d8d5f582fd97520c8d5bed87
patrick <mail@patricksanders.net>
2013-04-20 11:08:48 -0500

    added sample settings file

---
+++ muse/settings_SAMPLE.py
@@ -0,0 +1,8 @@
+# Rdio API credentials
+RDIO_CONSUMER_KEY = ''
+RDIO_CONSUMER_SECRET = ''
+
+# Echonest API credentials
+ECHONEST_API_KEY = ''
+ECHONEST_CONSUMER_KEY = ''
+ECHONEST_SHARED_SECRET = ''
