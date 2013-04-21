defa73f7fba5d88816214ac355874baece2969c1
patrick <mail@patricksanders.net>
2013-04-21 14:03:54 -0500

    Added more artist info like danceability and average song length

--- muse/muse.py
+++ muse/muse.py
@@ -47,18 +47,43 @@ class GetArtist(webapp2.RequestHandler):
 		
 		# Find artist on EchoNest
 		en_artist = artist.Artist(query)
-		# rdioalbums = rdio.call('getAlbumsForArtist', {'artist': firstResult})
 		images = en_artist.get_images(results=15)
 		image_url = images[random.randint(0,14)]['url']
-		artist_name = en_artist.name
 		hotttnesss = en_artist.hotttnesss * 50
 		familiarity = en_artist.familiarity * 50
+		similar_list = artist.similar(ids=en_artist.id, results=7)
+		song_list = en_artist.get_songs(results=25)
+		doc_counts = en_artist.get_doc_counts()
+		num_songs = len(song_list)
+		blog_list = en_artist.get_blogs(results=5, high_relevance=True)
+		
+		# Calculate average danceability
+		total_danceability = 0
+		for song in song_list:
+			total_danceability += song.get_audio_summary()['danceability']
+		danceability = (total_danceability / num_songs) * 100
+		
+		# Calculate total and average song length
+		total_song_length = 0
+		for song in song_list:
+			total_song_length += song.get_audio_summary()['duration']
+		total_song_length = total_song_length / 60
+		avg_song_length = total_song_length / num_songs
+		
 		
 		template_values = {
 			'image_url': image_url,
-			'artist_name': artist_name,
+			'artist_name': en_artist.name,
 			'hotttnesss': hotttnesss,
 			'familiarity': familiarity,
+			'danceability': danceability,
+			'term_list': en_artist.terms,
+			'similar_list': similar_list,
+			'total_songs': num_songs,
+			'total_song_length': total_song_length,
+			'avg_song_length': avg_song_length,
+			'blog_count': doc_counts['blogs'],
+			'blog_list': blog_list,
 		}
 		
 		template = JINJA_ENVIRONMENT.get_template('templates/artist.html')
