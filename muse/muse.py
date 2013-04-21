6a2bea2d2861c4562f5fd41e94231fd75d5d8d84
patrick <mail@patricksanders.net>
2013-04-21 10:53:48 -0500

    added 'Popular Now' to homepage with links to artist pages

--- muse/muse.py
+++ muse/muse.py
@@ -21,50 +21,21 @@ rdio = Rdio((RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET))
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
+		hot_list = artist.top_hottt(results=10)
+		
+		template_values = {
+			'hot_list': hot_list,
+		}
+		
 		template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-		self.response.write(template.render())
+		self.response.write(template.render(template_values))
 		
 class AboutPage(webapp2.RequestHandler):
 	def get(self):
-		# user = users.get_current_user()
-		# 
-		# if user:
-		# 	url = users.create_logout_url(self.request.uri)
-		# 	url_linktext = 'Logout'
-		# else:
-		# 	url = users.create_login_url(self.request.uri)
-		# 	url_linktext = 'Login'
-		# 
-		# template_values = {
-		# 	'url': url,
-		# 	'url_linktext': url_linktext,
-		# }
-
 		template = JINJA_ENVIRONMENT.get_template('templates/about.html')
 		self.response.write(template.render())
 
 class GetArtist(webapp2.RequestHandler):
-	# def post(self):
-	# 	query = self.request.get('artist')
-	# 	
-	# 	# Find artist on Rdio
-	# 	rdio = Rdio((RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET))
-	# 	searchResults = rdio.call('search', {'query': query,
-	# 									'types': 'Artist'
-	# 									})
-	# 	firstResult = searchResults['result']['results'][0]['key']
-	# 	
-	# 	# Find artist on EchoNest
-	# 	en_artist = artist.Artist(query)
-	# 	rdioalbums = rdio.call('getAlbumsForArtist', {'artist': firstResult})
-	# 	
-	# 	# Print results for EchoNest then Rdio
-	# 	self.response.headers['Content-Type'] = 'text/plain'
-	# 	self.response.write('First EchoNest result: ' + repr(en_artist) + '\n')
-	# 	self.response.write('First Rdio search result key: ' + firstResult + '\n')
-	# 	self.response.write('Albums for artist:\n')
-	# 	self.response.write(json.dumps(rdioalbums, ensure_ascii=True, indent=2))
-	
 	def get(self):
 		query = self.request.get('artist')
 		
@@ -92,13 +63,6 @@ class GetArtist(webapp2.RequestHandler):
 		
 		template = JINJA_ENVIRONMENT.get_template('templates/artist.html')
 		self.response.write(template.render(template_values))
-		
-		# Print results for EchoNest then Rdio
-		# self.response.headers['Content-Type'] = 'text/plain'
-		# self.response.write('First EchoNest result: ' + repr(en_artist) + '\n')
-		# self.response.write('First Rdio search result key: ' + firstResult + '\n')
-		# self.response.write('Albums for artist:\n')
-		# self.response.write(json.dumps(rdioalbums, ensure_ascii=True, indent=2))
 
 app = webapp2.WSGIApplication([('/', MainPage),
 								('/about', AboutPage),
